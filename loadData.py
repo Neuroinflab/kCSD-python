39045b307f210b2dba52c97e38fa30cd7c4e5e85
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2017-09-22 15:41:05 +0200

    Find paths of the morphology, LFP and electrode positions files

--- loadData.py
+++ loadData.py
@@ -12,9 +12,35 @@ class Data(object):
     def sub_dir_path(self,d):
         return  filter(os.path.isdir, [os.path.join(d,f) for f in os.listdir(d)])
 
+    def get_fname(self,d,fnames):
+
+        if len(fnames) == 1:
+            return os.path.join(d,fnames[0])
+        else:
+            paths = []
+            for fname in fnames:
+                paths.append(os.path.join(d,fname))
+            return paths
+        
     def get_paths(self):
+
         dir_list = self.sub_dir_path(self.path)
+        for drc in dir_list:
+            files = os.listdir(drc)
+            if drc.endswith("morphology"):
+                self.path_morphology = self.get_fname(drc,files)
+            if drc.endswith("positions"):
+                self.path_ele_pos = self.get_fname(drc,files)
+            if drc.endswith("LFP"):
+                self.path_LFP = self.get_fname(drc,files)
+                   
+        
+        
     def __init__(self,path):
 
         self.path = path
         self.get_paths()
+
+
+if __name__ == '__main__':
+    obj = Data("Data/gang_7x7_200")
--- tests/test_loadData.py
+++ tests/test_loadData.py
@@ -6,9 +6,15 @@ import os
 import unittest
 import utility_functions as utils
 
+try:
+  basestring
+except NameError:
+  basestring = str
+
 sys.path.append('..')
 
 from loadData import Data
+
 class testData(unittest.TestCase):
     
     def setUp(self):
@@ -16,6 +22,22 @@ class testData(unittest.TestCase):
     
     def test_path_expansion_morphology(self):
         self.assertEqual(1,'Data/gang_7x7_200/morphology' in self.data.sub_dir_path(self.data.path))
-    
+        
+    def test_get_fname_string(self):
+        self.assertTrue(isinstance(self.data.get_fname('Data/gang_7x7_200/LFP',['myLFP']),basestring))
+
+    def test_get_fname_list(self):
+        self.assertTrue(isinstance(self.data.get_fname('Data/gang_7x7_200/LFP',['myLFP','yourLFP']),list))
+
+    def test_get_paths_LFP(self):
+        self.assertTrue('Data/gang_7x7_200/LFP/myLFP',self.data.path_LFP)
+        
+    def test_get_paths_morpho(self):
+        self.assertTrue('Data/gang_7x7_200/morphology/Badea2011Fig2Du.CNG.swc',self.data.path_morphology)
+        
+    def test_get_paths_ele_pos(self):
+        self.assertTrue('Data/gang_7x7_200/LFP/electrode_positions/elcoord_x_y_z',self.data.path_ele_pos)
+
+        
 if __name__ == '__main__':
   unittest.main()
