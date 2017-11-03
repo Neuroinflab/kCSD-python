8147e71967a7c757c56f45e12c86660f038ce3d0
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2017-11-03 16:21:28 +0100

    Calculate kernels for ball and stick

--- sKCSD3D.py
+++ sKCSD3D.py
@@ -315,23 +315,30 @@ class sKCSD3D(KCSD3D):
 
 if __name__ == '__main__':
     import loadData as ld
-    data_dir = "Data/gang_7x7_200"
+    data_dir = "Data/ball_and_stick_8"
     data = ld.Data(data_dir)
     scaling_factor = 100
-    ele_pos = data.ele_pos/scaling_factor
-    pots = data.LFP
+    ele_pos = data.ele_pos/100
+    pots = data.LFP[:,:200]
     params = {}
     morphology = data.morphology 
-    morphology[:,2:6] = morphology[:,2:6]/scaling_factor
-    xmin, ymin, zmin, xmax,ymax,zmax = -4.3251,-4.8632,0.,4.4831,6.3881,0.875
-    gdx = (xmax-xmin)/50
-    gdy = (ymax-ymin)/50
-    gdz = (zmax-zmin)/100
+    morphology[:,2:6] = morphology[:,2:6]/100
+    xmin, ymin, zmin, xmax,ymax,zmax = -4.3251, -4.8632, 0., 4.4831, 6.3881, 0.875
+    xmin = morphology[:,2].min()-morphology[:,5].max()
+    xmax = morphology[:,2].max()+morphology[:,5].max()
+    ymin = morphology[:,3].min()-morphology[:,5].max()
+    ymax = morphology[:,3].max()+morphology[:,5].max()
+    zmin = morphology[:,4].min()-morphology[:,5].max()
+    zmax = morphology[:,4].max()+morphology[:,5].max()
+    print(xmin,xmax,ymin,ymax,zmin,zmax)
+    gdx = (xmax-xmin)/88
+    gdy = (ymax-ymin)/112
+    gdz = (zmax-zmin)/20
     k = sKCSD3D(ele_pos, pots,morphology,
                gdx=gdx, gdy=gdy, gdz=gdz,
                xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, zmin=zmin, zmax=zmax,
-               n_src_init=1000, src_type='gauss_lim')
-    k.cross_validate()
+               n_src_init=10, src_type='gauss_lim')
+    #k.cross_validate()
     
     if sys.version_info >= (3, 0):
         path = os.path.join(data_dir,"preprocessed_data/Python_3")
@@ -345,7 +352,6 @@ if __name__ == '__main__':
     utils.save_sim(path,k)
     #est_csd = k.values("CSD")
     #est_pot = k.values("POT")
-
     
     
     
