1f668c72629c629284a3a3475daffa37632b1edf
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2017-11-07 12:16:48 +0100

    Add tstop

--- make_fig_2.py
+++ make_fig_2.py
@@ -5,16 +5,19 @@ import utility_functions as utils
 import sys
 import os
 import loadData as ld
+import numpy as np
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     fname_base = "ball_stick_"
     paths = []
+    tstop = 75
     for rownb in [8,32,128]:
         fname = fname_base+str(rownb)
-        c = run_LFP.CellModel(morphology=1,cell_name=fname,colnb=1,rownb=rownb,xmin=-100,xmax=600,ymin=0,ymax=200)
+        c = run_LFP.CellModel(morphology=1,cell_name=fname,colnb=1,rownb=rownb,xmin=-100,xmax=600,ymin=0,ymax=200,tstop=tstop)
         c.simulate()
         c.save_skCSD_python()
-    
+        c.save_memb_curr()
         data_dir = c.return_paths_skCSD_python()
     
         data = ld.Data(data_dir)
@@ -23,18 +26,17 @@ if __name__ == '__main__':
         pots = data.LFP[:,:200]
         params = {}
         morphology = data.morphology
-        print(morphology)
         xmin = morphology[:,2].min()-morphology[:,5].max()
         xmax = morphology[:,2].max()+morphology[:,5].max()
         ymin = morphology[:,3].min()-morphology[:,5].max()
         ymax = morphology[:,3].max()+morphology[:,5].max()
         zmin = morphology[:,4].min()-morphology[:,5].max()
         zmax = morphology[:,4].max()+morphology[:,5].max()
-        print(xmin,xmax,ymin,ymax,zmin,zmax)
+ 
         gdx = (xmax-xmin)/10
         gdy = (ymax-ymin)/10
-        gdz = (zmax-zmin)/100
-        k = sKCSD3D.sKCSD3D(ele_pos, pots,morphology, gdx=gdx, gdy=gdy, gdz=gdz, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, zmin=zmin, zmax=zmax, n_src_init=10, src_type='gauss_lim')
+        gdz = (zmax-zmin)/200
+        k = sKCSD3D.sKCSD3D(ele_pos, pots,morphology, gdx=gdx, gdy=gdy, gdz=gdz, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, zmin=zmin, zmax=zmax, n_src_init=512, src_type='gauss_lim')
         #k.cross_validate()
     
         if sys.version_info >= (3, 0):
@@ -48,3 +50,28 @@ if __name__ == '__main__':
         
         utils.save_sim(path,k)
         paths.append(path)
+        
+    ground_truth_name = os.path.join(data_dir,'membcurr')
+    ground_truth = np.loadtxt(ground_truth_name)
+    vmin,vmax = ground_truth.min(),ground_truth.max()
+    if vmin < 0 and vmax > 0:
+        if abs(vmax) > abs(vmin):
+            vmin = -vmax
+        else:
+            vmax = -vmin
+            
+    print(ground_truth.shape)
+    fig = plt.figure()
+    ax = []
+    for i in range(6):
+        if i != 1:
+            ax.append(fig.add_subplot(2,3,i+1))
+        if not i:
+            cax = ax[0].imshow(ground_truth,extent=[0, tstop,1, 52,],origin='lower',aspect='auto',cmap='bwr',vmax=vmax,vmin=vmin)
+            fig.colorbar(cax)
+        
+    
+
+
+    
+    plt.show()
