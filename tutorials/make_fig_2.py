da8b8cd4cb34b3f96c236f102a554a25ec6f11d0
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2018-03-01 13:41:50 +0100

    Code refactoring

--- tutorials/make_fig_2.py
+++ tutorials/make_fig_2.py
@@ -10,110 +10,77 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from corelib import sKCSD3D
 import corelib.utility_functions as utils
 import corelib.loadData as ld
+import functions as fun
 
 n_src = 512
+
 if __name__ == '__main__':
-    fname_base = "ball_stick_"
+    fname_base = "ball_stick_random"
     
     tstop = 75
     scaling_factor = 1000**2
     scaling_factor_LFP = 1000
     R_inits = [(2**i)/np.sqrt(2) for i in [3,4,5,6,7]]
     electrode_number = [8,16,32,64,128]
-   
+    data_dir = []
+    
+    for rownb in electrode_number:
+        fname = fname_base+str(rownb)
+        c = run_LFP.CellModel(morphology=1,cell_name=fname,colnb=1,rownb=rownb,xmin=-100,xmax=600,ymin=0,ymax=200,tstop=tstop,seed=1988,weight=0.01,n_syn=100)
+        c.simulate()
+        c.save_skCSD_python()
+        c.save_memb_curr()
+        c.save_seg_length()
+        data_dir.append(c.return_paths_skCSD_python())
+        
+    seglen = np.loadtxt(os.path.join(data_dir[0],'seglength'))#/scaling_factor
+    ground_truth = np.loadtxt(os.path.join(data_dir[0],'membcurr'))
+    ground_truth = ground_truth/seglen[:,None]
+    gvmin, gvmax = fun.get_min_max(ground_truth)
     
     for lambd in [1e-5,1e-4,1e-3,1e-2,1e-1]:
         for R_init in R_inits:
             simulation_paths = []
             data_paths = []
-            fig_lfp = plt.figure()
-            ax_lfp = []
-            l = 0
-            for rownb in electrode_number:
-                fname = fname_base+str(rownb)
-                c = run_LFP.CellModel(morphology=1,cell_name=fname,colnb=1,rownb=rownb,xmin=-100,xmax=600,ymin=0,ymax=200,tstop=tstop,seed=1988,weight=0.01,n_syn=100)
-                c.simulate()
-                c.save_skCSD_python()
-                c.save_memb_curr()
-                c.save_seg_length()
-                data_dir = c.return_paths_skCSD_python()
-                data = ld.Data(data_dir)
- 
+            fig = plt.figure()
+            ax = []
+            for i, datd in enumerate(data_dir):
+                l = 0
+                data = ld.Data(datd)
                 ele_pos = data.ele_pos/scaling_factor
-
                 pots = data.LFP/scaling_factor_LFP
-                ax_lfp.append(fig_lfp.add_subplot(1,5,l+1))
-                x = ax_lfp[l].imshow(data.LFP,extent=[0, tstop,1, 52,],origin='lower',aspect='auto',cmap='bwr')
-                
-                l = l+1
-  
-                params = {}
                 morphology = data.morphology
                 morphology[:,2:6] = morphology[:,2:6]/scaling_factor
                 R = R_init/scaling_factor
                
-                #print(ele_pos,'\n',R,'\n',morphology[:,2:5])
-                
                 k = sKCSD3D.sKCSD3D(ele_pos,data.LFP,morphology, n_src_init=n_src, src_type='gauss',lambd=lambd,R_init=R)
-                #k.cross_validate()
+                print(k.cross_validate(lambdas=np.array([lambd])))
+                est_csd = k.values()
                 dir_name = "ball_stick_random_R_"+str(R_init)+'_lambda_'+str(lambd)+'_src_'+str(n_src)
+                
                 if sys.version_info >= (3, 0):
-                    path = os.path.join(data_dir,"preprocessed_data/Python_3", dir_name)
+                    new_path = os.path.join(datd,"preprocessed_data/Python_3", dir_name)
                 else:
-                    path = os.path.join(data_dir,"preprocessed_data/Python_2",dir_name)
+                    new_path = os.path.join(datd,"preprocessed_data/Python_2",dir_name)
                 
-                if not os.path.exists(path):
-                    print("Creating",path)
-                    os.makedirs(path)
+                if not os.path.exists(new_path):
+                    print("Creating",new_path)
+                    os.makedirs(new_path)
         
-                utils.save_sim(path,k)
-                simulation_paths.append(path)
-                data_paths.append(data_dir)
-               
-            #data_paths = ['simulation/ball_stick_8','simulation/ball_stick_32','simulation/ball_stick_128']
-            #simulation_paths = ['simulation/ball_stick_8/preprocessed_data/Python_2','simulation/ball_stick_32/preprocessed_data/Python_2','simulation/ball_stick_128/preprocessed_data/Python_2']
-
-            seglen = np.loadtxt(os.path.join(data_paths[0],'seglength'))#/scaling_factor
-            ground_truth = np.loadtxt(os.path.join(data_paths[0],'membcurr'))
-            LFP = pots
-   
-            ground_truth = ground_truth/seglen[:,None]
-            print(ground_truth.max(),ground_truth.min())
-                
-            fig = plt.figure()
-            ax = []
-            fig2 = plt.figure()
-            ax2 = []
-            for i in range(6):
+                utils.save_sim(new_path,k)
+                simulation_paths.append(new_path)
                 ax.append(fig.add_subplot(2,3,i+1))
-                if i < 5:
-                    ax2.append(fig2.add_subplot(1,5,i+1))
-            
                 if not i:
-                    vmin,vmax = ground_truth.min(),ground_truth.max()
-                    if vmin < 0 and vmax > 0:
-                        if abs(vmax) > abs(vmin):
-                            vmin = -vmax
-                        else:
-                            vmax = -vmin
-                    cax = ax[0].imshow(ground_truth,extent=[0, tstop,1, 52,],origin='lower',aspect='auto',cmap='seismic_r',vmax=vmax,vmin=vmin)
-                    cbar = fig.colorbar(cax,ticks=[vmin,vmax])
+                    cax = ax[0].imshow(ground_truth,extent=[0, tstop,1, 52,],origin='lower',aspect='auto',cmap='seismic_r',vmax=gvmax,vmin=gvmin)
+                    cbar = fig.colorbar(cax,ticks=[gvmin,gvmax])
                     cbar.ax.set_yticklabels(['source','sink'])
-          
-
                 else:
-                    path = simulation_paths[i-1]
-                    data_dir = data_paths[i-1]
-                    data = ld.Data(data_dir)
-                    est_csd, est_pot, cell_obj = utils.load_sim(path)
                     new_csd = np.zeros(est_csd.shape[2:])
-                    new_pot = np.zeros(est_pot.shape[2:])
                     zdim = est_csd.shape[2]
                     tot_len = sum(seglen)
                     for k in range(new_csd.shape[0]):
                         for j in range(new_csd.shape[1]):
                             new_csd[k,j] = est_csd[:,:,k,j].sum()/(est_csd.shape[0]*est_csd.shape[1])*(np.pi)**0.5*R_inits[i-1]
-                            new_pot[k,j] = est_pot[:,:,k,j].sum()/(est_pot.shape[0]*est_pot.shape[1])
                  
                     csd = np.zeros(ground_truth.shape)
                     where_am_I = 0
@@ -121,26 +88,16 @@ if __name__ == '__main__':
                         if j == len(seglen)-1:
                             csd[j,:] = new_csd[where_am_I:,:].sum(axis=0)/seg
                         else:
-                            no_vox = round(seg*zdim/tot_len)
+                            no_vox = int(round(seg*zdim/tot_len))
                             csd[j,:] = new_csd[where_am_I:where_am_I+no_vox,:].sum(axis=0)/seg
                             where_am_I += no_vox
             
                     
-                    vmin,vmax = csd.min(),csd.max()
-                    print(vmin,vmax)
-                    if vmin*vmax < 0:
-                        if abs(vmax) > abs(vmin):
-                            vmin = -vmax
-                        else:
-                            vmax = -vmin
+                    
                         
-                        
+                    vmin, vmax = fun.get_min_max(csd)
                     cax = ax[i].imshow(csd,extent=[0, tstop,1, 52,],origin='lower',aspect='auto',cmap='seismic_r',vmax=vmax,vmin=vmin)
                     cbar = fig.colorbar(cax,ticks=[vmin,vmax])
-                    
-                    
-                    cax2 = ax2[i-1].imshow(new_pot,extent=[0, tstop,1, 52,],origin='lower',aspect='auto',cmap='seismic_r')#,vmax=vmax,vmin=vmin)
-
                     ax[i].set_title(electrode_number[i-1])
                     cbar.ax.set_yticklabels(['source','sink'])
     
