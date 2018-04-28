ae3697d3a252df978b38883981a0fd80f0637968
Chaitanya Chintaluri <ccluri@gmail.com>
2018-04-28 15:58:15 +0100

    moving sKCSD stuff around

--- kcsd/__init__.py
+++ kcsd/__init__.py
@@ -1,5 +1,5 @@
 from .KCSD import KCSD1D, KCSD2D, KCSD3D, MoIKCSD
-from . import sKCSD, sKCSDcell
+from . import sKCSD
 from .validation import csd_profile
 from .validation.ValidateKCSD import ValidateKCSD, ValidateKCSD1D, ValidateKCSD2D, ValidateKCSD3D, SpectralStructure
 from .validation.VisibilityMap import VisibilityMap1D, VisibilityMap2D, VisibilityMap3D
