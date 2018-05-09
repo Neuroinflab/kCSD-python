d653c7cdfcb879ee378897d7d2a6464cff0b687a
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2018-05-09 13:27:43 +0200

    Pep8

--- sKCSD_paper/make_fig_2.py
+++ sKCSD_paper/make_fig_2.py
@@ -1,5 +1,4 @@
 from __future__ import division, print_function
-
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
@@ -7,9 +6,8 @@ import os
 from distutils.spawn import find_executable, spawn
 import shutil
 import subprocess
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 if find_executable('nrnivmodl') is not None:
     for path in ['x86_64', 'i686', 'powerpc']:
         if os.path.isdir(path):
@@ -17,8 +15,8 @@ if find_executable('nrnivmodl') is not None:
     spawn([find_executable('nrnivmodl')])
     subprocess.call(["nrnivmodl", "sinsyn.mod"])
 else:
-    print("nrnivmodl script not found in PATH, thus NEURON .mod files could" +
-"not be compiled, and LFPy.test() functions will fail")
+    print("""nrnivmodl script not found in PATH, thus NEURON .mod files could
+    not be compiled, and LFPy.test() functions will fail""")
 
 
 from corelib import sKCSD
@@ -39,49 +37,73 @@ if __name__ == '__main__':
     tstop = 850
     scaling_factor = 1000**2
     scaling_factor_LFP = 1000
-    electrode_number = [8,16,128]
+    electrode_number = [8, 16, 128]
     data_dir = []
     xmin, xmax = -100, 600
     ymin, ymax = 0, 200
     orientation = 1
     for rownb in electrode_number:
         fname = "Figure_2"
-        c = sKCSD_utils.simulate(fname,morphology=1,simulate_what="sine",colnb=1,rownb=rownb,xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax,tstop=tstop,seed=1988,weight=0.1,n_syn=100)
-        
+        c = sKCSD_utils.simulate(fname,
+                                 morphology=1,
+                                 simulate_what="sine",
+                                 colnb=1,
+                                 rownb=rownb,
+                                 xmin=xmin,
+                                 xmax=xmax,
+                                 ymin=ymin,
+                                 ymax=ymax,
+                                 tstop=tstop,
+                                 seed=1988,
+                                 weight=0.1,
+                                 n_syn=100)
         data_dir.append(c.return_paths_skCSD_python())
-        
-    seglen = np.loadtxt(os.path.join(data_dir[0],'seglength'))
-    ground_truth = np.loadtxt(os.path.join(data_dir[0],'membcurr'))
-    ground_truth = ground_truth/seglen[:,None]*1e-3
+    seglen = np.loadtxt(os.path.join(data_dir[0], 'seglength'))
+    ground_truth = np.loadtxt(os.path.join(data_dir[0], 'membcurr'))
+    ground_truth = ground_truth/seglen[:, None]*1e-3
     gvmin, gvmax = pl.get_min_max(ground_truth)
-    fname = fname_base+'.png'
+    fname = fname_base + '.png'
     fig_name = sKCSD_utils.make_fig_names(fname)
     data_paths = []
-    fig, ax = plt.subplots(4,1)
-    xticklabels = list(np.linspace(0,800,5))
-    yticklabels = list(np.linspace(0,52,5))
-    pl.plot(ax[0],ground_truth)
+    fig, ax = plt.subplots(4, 1)
+    xticklabels = list(np.linspace(0, 800, 5))
+    yticklabels = list(np.linspace(0, 52, 5))
+    pl.plot(ax[0], ground_truth)
     for i, datd in enumerate(data_dir):
         l = 0
         data = ld.Data(datd)
         ele_pos = data.ele_pos/scaling_factor
         data.LFP = data.LFP/scaling_factor_LFP
         morphology = data.morphology
-        morphology[:,2:6] = morphology[:,2:6]/scaling_factor
-        k = sKCSD.sKCSD(ele_pos,data.LFP,morphology, n_src_init=n_src, src_type='gauss',lambd=lambd,R_init=R)
-        est_csd = k.values(transformation='segments')/seglen[:,None]
+        morphology[:, 2:6] = morphology[:, 2:6]/scaling_factor
+        k = sKCSD.sKCSD(ele_pos,
+                        data.LFP,
+                        morphology,
+                        n_src_init=n_src,
+                        src_type='gauss',
+                        lambd=lambd,
+                        R_init=R)
+        est_csd = k.values(transformation='segments')/seglen[:, None]
         if i == 2:
-            pl.plot(ax[i+1],est_csd,xticklabels=xticklabels,yticklabels=yticklabels,xlabel='Time [ms]',ylabel='#segment')
+            pl.plot(ax[i+1],
+                    est_csd,
+                    xticklabels=xticklabels,
+                    yticklabels=yticklabels,
+                    xlabel='Time [ms]',
+                    ylabel='#segment')
         else:
-            pl.plot(ax[i+1],est_csd)
-            
-        if sys.version_info < (3,0):
+            pl.plot(ax[i+1], est_csd)
+
+        if sys.version_info < (3, 0):
             path = os.path.join(datd, "preprocessed_data/Python_2")
         else:
             path = os.path.join(datd, "preprocessed_data/Python_3")
 
         if not os.path.exists(path):
-            print("Creating",path)
+            print("Creating", path)
             os.makedirs(path)
-        utils.save_sim(path,k)
-    fig.savefig(fig_name, bbox_inches='tight', transparent=True, pad_inches=0.1)
+        utils.save_sim(path, k)
+    fig.savefig(fig_name,
+                bbox_inches='tight',
+                transparent=True,
+                pad_inches=0.1)
--- sKCSD_paper/make_fig_3.py
+++ sKCSD_paper/make_fig_3.py
@@ -4,63 +4,84 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from corelib import sKCSD
 sKCSD.skmonaco_available = False
 import corelib.utility_functions as utils
 import loadData as ld
 import validation.plotting_functions as pl
 import sKCSD_utils
-
 n_src = 512
 lambd = 1
 R = 8e-6/2**.5
-
 if __name__ == '__main__':
     fname_base = "Figure_3"
-
     tstop = 75
     scaling_factor = 1000**2
     scaling_factor_LFP = 1000
-    R_inits = [2**i for i in range(3,8)]
+    R_inits = [2**i for i in range(3, 8)]
     lambdas = [10**(-i) for i in range(6)]
-    electrode_number = [8,32,128]
+    electrode_number = [8, 32, 128]
     data_dir = []
     xmin, xmax = -100, 600
     ymin, ymax = 0, 200
     orientation = 1
     for rownb in electrode_number:
         fname = "Figure_3"
-        c = sKCSD_utils.simulate(fname,morphology=1,simulate_what="random",colnb=1,rownb=rownb,xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax,tstop=tstop,seed=1988,weight=0.1,n_syn=100)
+        c = sKCSD_utils.simulate(fname,
+                                 morphology=1,
+                                 simulate_what="random",
+                                 colnb=1,
+                                 rownb=rownb,
+                                 xmin=xmin,
+                                 xmax=xmax,
+                                 ymin=ymin,
+                                 ymax=ymax,
+                                 tstop=tstop,
+                                 seed=1988,
+                                 weight=0.1,
+                                 n_syn=100)
         data_dir.append(c.return_paths_skCSD_python())
-        
-    seglen = np.loadtxt(os.path.join(data_dir[0],'seglength'))#/scaling_factor
-    ground_truth = np.loadtxt(os.path.join(data_dir[0],'membcurr'))
-    ground_truth = ground_truth/seglen[:,None]*1e-3
+    seglen = np.loadtxt(os.path.join(data_dir[0], 'seglength'))
+    ground_truth = np.loadtxt(os.path.join(data_dir[0], 'membcurr'))
+    ground_truth = ground_truth/seglen[:, None]*1e-3
     gvmax, gvmin = pl.get_min_max(ground_truth)
-    
     data_paths = []
-    fig, ax = plt.subplots(2,3)
-    cax = ax[0,0].imshow(ground_truth,extent=[0, tstop,1, 52,],origin='lower',aspect='auto',cmap='seismic_r',vmax=gvmax,vmin=gvmin)
-    new_fname = fname_base+'.png'
+    fig, ax = plt.subplots(2, 3)
+    cax = ax[0, 0].imshow(ground_truth,
+                          extent=[0, tstop, 1, 52],
+                          origin='lower',
+                          aspect='auto',
+                          cmap='seismic_r',
+                          vmax=gvmax,
+                          vmin=gvmin)
+    new_fname = fname_base + '.png'
     fig_name = sKCSD_utils.make_fig_names(new_fname)
-    
     for i, datd in enumerate(data_dir):
         data = ld.Data(datd)
         ele_pos = data.ele_pos/scaling_factor
         data.LFP = data.LFP/scaling_factor_LFP
         morphology = data.morphology
-        morphology[:,2:6] = morphology[:,2:6]/scaling_factor
-        k = sKCSD.sKCSD(ele_pos,data.LFP,morphology, n_src_init=n_src, src_type='gauss',lambd=lambd,R_init=R)
+        morphology[:, 2:6] = morphology[:, 2:6]/scaling_factor
+        k = sKCSD.sKCSD(ele_pos,
+                        data.LFP,
+                        morphology,
+                        n_src_init=n_src,
+                        src_type='gauss',
+                        lambd=lambd,
+                        R_init=R)
         csd = k.values(transformation='segments')
-                
-        
         vmax, vmin = pl.get_min_max(csd)
-        cax = ax[1,i].imshow(csd,extent=[0, tstop,1, 52,],origin='lower',aspect='auto',cmap='seismic_r',vmax=vmax,vmin=vmin)
-        #cbar = fig.colorbar(cax,ticks=[vmin,vmax])
-        ax[1,i].set_title(electrode_number[i])
-        #cbar.ax.set_yticklabels(['source','sink'])
-        
-    fig.savefig(fig_name, bbox_inches='tight', transparent=True, pad_inches=0.1)
+        cax = ax[1, i].imshow(csd,
+                              extent=[0, tstop, 1, 52],
+                              origin='lower',
+                              aspect='auto',
+                              cmap='seismic_r',
+                              vmax=vmax,
+                              vmin=vmin)
+        ax[1, i].set_title(electrode_number[i])
+    fig.savefig(fig_name,
+                bbox_inches='tight',
+                transparent=True,
+                pad_inches=0.1)
