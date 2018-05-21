2fee44d56173e0cd2847790921470393b491b59b
Chaitanya Chintaluri <ccluri@gmail.com>
2018-05-21 19:06:31 +0100

    continuous_integration/install.sh

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -118,4 +118,4 @@ fi
 pip install .   # Installs kcsd-python 
 python -c "import numpy; import os; assert os.getenv('NUMPY_VERSION') == numpy.__version__"
 python -c "import scipy; import os; assert os.getenv('SCIPY_VERSION') == scipy.__version__"
-python -c "import kcsd; assert kcsd.__version__ == 1.2"
+
