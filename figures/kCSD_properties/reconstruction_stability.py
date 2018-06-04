a3ac10e5e91daac8150521016bbe4c3a62df7181
m-kowalska <m.kowalska@nencki.gov.pl>
2018-06-04 15:31:27 +0200

    docstrings for reconstruction stability

--- figures/kCSD_properties/reconstruction_stability.py
+++ figures/kCSD_properties/reconstruction_stability.py
@@ -52,59 +52,82 @@ def save_source_code(save_path, timestr):
         sf.write(open(__file__).read())
 
 
-home = expanduser('~')
-DAY = datetime.datetime.now()
-DAY = DAY.strftime('%Y%m%d')
-TIMESTR = time.strftime("%H%M%S")
-SAVE_PATH = home + "/kCSD_results/" + DAY + '/' + TIMESTR
-makemydir(SAVE_PATH)
-save_source_code(SAVE_PATH, time.strftime("%Y%m%d-%H%M%S"))
-
-CSD_PROFILE = CSD.gauss_1d_mono
-CSD_SEED = 15
-N_SRC = [2, 4, 8, 16, 32, 64, 128, 256, 512]
-ELE_LIMS = [0.1, 0.9]  # range of electrodes space
-TRUE_CSD_XLIMS = [0., 1.]
-TOTAL_ELE = 10
-
-
 def stability_M(csd_profile, csd_seed, n_src, ele_lims, true_csd_xlims,
                 total_ele):
+    """
+    Investigates stability of reconstruction for different number of basis
+    sources
+
+    Parameters
+    ----------
+    csd_profile: function
+        Function to produce csd profile.
+    csd_seed: int
+        Seed for random generator to choose random CSD profile.
+    n_src: int
+        Number of basis sources.
+    ele_lims: list
+        Boundaries for electrodes placement.
+    true_csd_xlims: list
+        Boundaries for ground truth space.
+    total_ele: int
+        Number of electrodes.
+    Returns
+    -------
+    obj_all: class object
+    rms: float
+        Normalized error of reconstruction.
+    point_error_all: numpy array
+        Normalized error of reconstruction calculated separetly at every point
+        point of estimation space.
+    eigenvalues: numpy array
+        Eigenvalues of k_pot matrix.
+    eigenvectors: numpy array
+        Eigen vectors of k_pot matrix.
+    """
     obj_all = []
     rms = np.zeros((len(n_src)))
     point_error_all = []
     eigenvectors = np.zeros((len(n_src), total_ele, total_ele))
     eigenvalues = np.zeros((len(n_src), total_ele))
-    for indx, i in enumerate(n_src):
-        KK = ValidateKCSD1D(csd_seed, n_src_init=i, R_init=0.23,
+    for i, value in enumerate(n_src):
+        KK = ValidateKCSD1D(csd_seed, n_src_init=value, R_init=0.23,
                             ele_lims=ele_lims, true_csd_xlims=true_csd_xlims,
                             sigma=0.3, h=0.25, src_type='gauss')
-        obj, rms[indx], point_error = KK.make_reconstruction(csd_profile,
-                                                             csd_seed,
-                                                             total_ele=total_ele,
-                                                             noise=0,
-                                                             Rs=np.arange(0.2, 0.5, 0.1))
+        obj, rms[i], point_error = KK.make_reconstruction(csd_profile,
+                                                          csd_seed,
+                                                          total_ele=total_ele,
+                                                          noise=0,
+                                                          Rs=np.arange(0.2,
+                                                                       0.5,
+                                                                       0.1))
         ss = SpectralStructure(obj)
-        eigenvectors[indx], eigenvalues[indx] = ss.evd()
+        eigenvectors[i], eigenvalues[i] = ss.evd()
         point_error_all.append(point_error)
         obj_all.append(obj)
     return obj_all, rms, point_error_all, eigenvalues, eigenvectors
 
 
-OBJ, RMS, POINT_ERROR, eigenval, eigenvec = stability_M(CSD_PROFILE, CSD_SEED,
-                                                        N_SRC, ELE_LIMS,
-                                                        TRUE_CSD_XLIMS,
-                                                        TOTAL_ELE)
-k_pot_list = []
-k_interp_cross_list = []
-lambd = []
-for i in range(len(OBJ)):
-    k_pot_list.append(OBJ[i].k_pot)
-    k_interp_cross_list.append(OBJ[i].k_interp_cross)
-    lambd.append(OBJ[i].lambd)
-
-
 def plot_M(n_src_init, rms, save_path):
+    """
+    Creates plot of relationship between RMS error and different number of
+    basis sources
+
+    Parameters
+    ----------
+    n_src_init: list
+        List of number of basis sources.
+    rms: numpy array
+        Error of reconstruction.
+    save_path: string
+        Directory.
+    timestr: float
+        Time.
+
+    Returns
+    -------
+    None
+    """
     fig = plt.figure()
     plt.plot(n_src_init, rms, '--', marker='.')
     plt.xscale('log')
@@ -119,6 +142,23 @@ def plot_M(n_src_init, rms, save_path):
 
 
 def plot_eigenvalues(eigenvalues, save_path, n_src):
+    """
+    Creates plot of eigenvalues of kernel matrix (k_pot) for different number
+    of basis sources
+
+    Parameters
+    ----------
+    eigenvalues: numpy array
+        Eigenvalues of k_pot matrix.
+    save_path: string
+        Directory.
+    n_src: list
+        List of number of basis sources.
+
+    Returns
+    -------
+    None
+    """
     fig = plt.figure()
     for indx, i in enumerate(n_src):
         plt.plot(eigenvalues[indx], '--', marker='.', label='M='+str(i))
@@ -133,6 +173,23 @@ def plot_eigenvalues(eigenvalues, save_path, n_src):
 
 
 def plot_eigenvectors(eigenvectors, save_path, n_src):
+    """
+    Creates plot of eigenvectors of kernel matrix (k_pot) for different number
+    of basis sources
+
+    Parameters
+    ----------
+    eigenvectors: numpy array
+        Eigenvectors of k_pot matrix.
+    save_path: string
+        Directory.
+    n_src: list
+        List of number of basis sources.
+
+    Returns
+    -------
+    None
+    """
     fig = plt.figure(figsize=(15, 15))
     plt.suptitle('Eigenvalue decomposition of kernel matrix for different '
                  'number of basis sources')
@@ -152,6 +209,23 @@ def plot_eigenvectors(eigenvectors, save_path, n_src):
 
 
 def plot_k_interp_cross(k_icross, save_path, n_src):
+    """
+    Creates plot of vectors of cross kernel matrix (k_interp_cross) for
+    different number of basis sources
+
+    Parameters
+    ----------
+    k_icross: numpy array
+        List of cross kernel matrixes for different number of basis sources.
+    save_path: string
+        Directory.
+    n_src: list
+        List of number of basis sources.
+
+    Returns
+    -------
+    None
+    """
     fig = plt.figure(figsize=(k_icross[0].shape[1] + 5,
                               k_icross[0].shape[1] + 5))
     plt.suptitle('Vectors of cross kernel matrix for different number '
@@ -174,6 +248,25 @@ def plot_k_interp_cross(k_icross, save_path, n_src):
 
 
 def plot_eigenvalue_lambda(eigenvalues, lambd, save_path, n_src):
+    """
+    Creates plot of eigenvalues of kernel matrix (k_pot) with lambda for
+    different number of basis sources
+
+    Parameters
+    ----------
+    eigenvalues: numpy array
+        Eigenvalues of k_pot matrix.
+    lambd: list
+        Regularization parameter.
+    save_path: string
+        Directory.
+    n_src: list
+        List of number of basis sources.
+
+    Returns
+    -------
+    None
+    """
     fig = plt.figure(figsize=(7, 7))
     x = np.arange(1, eigenvalues.shape[1] + 1)
     for indx, i in enumerate(n_src):
@@ -193,6 +286,25 @@ def plot_eigenvalue_lambda(eigenvalues, lambd, save_path, n_src):
 
 
 def plot_k_interp_cross_v(k_icross, eigenvectors, save_path, n_src):
+    """
+    Creates plot of product of cross kernel vectors and eigenvectors for
+    different number of basis sources
+
+    Parameters
+    ----------
+    k_icross: numpy array
+        List of cross kernel matrixes for different number of basis sources.
+    eigenvectors: numpy array
+        Eigenvectors of k_pot matrix.
+    save_path: string
+        Directory.
+    n_src: list
+        List of number of basis sources.
+
+    Returns
+    -------
+    None
+    """
     fig = plt.figure(figsize=(k_icross[0].shape[1] + 5,
                               k_icross[0].shape[1] + 5))
     plt.suptitle('Vectors of cross kernel and eigenvectors product for '
@@ -215,6 +327,23 @@ def plot_k_interp_cross_v(k_icross, eigenvectors, save_path, n_src):
 
 
 def plot_k_pot(k_pot, save_path, n_src):
+    """
+    Creates plot of vectors of kernel matrix (k_pot) for
+    different number of basis sources
+
+    Parameters
+    ----------
+    k_pot: numpy array
+        List of kernel matrixes for different number of basis sources.
+    save_path: string
+        Directory.
+    n_src: list
+        List of number of basis sources.
+
+    Returns
+    -------
+    None
+    """
     fig = plt.figure(figsize=(k_pot[0].shape[1] + 5,
                               k_pot[0].shape[1] + 5))
     plt.suptitle('Vectors of kernel matrix for different number '
@@ -236,10 +365,38 @@ def plot_k_pot(k_pot, save_path, n_src):
     plt.close()
 
 
-plot_M(N_SRC, RMS, SAVE_PATH)
-plot_eigenvalues(eigenval, SAVE_PATH, N_SRC)
-plot_eigenvectors(eigenvec, SAVE_PATH, N_SRC)
-plot_k_interp_cross(k_interp_cross_list, SAVE_PATH, N_SRC)
-plot_k_interp_cross_v(k_interp_cross_list, eigenvec, SAVE_PATH, N_SRC)
-plot_eigenvalue_lambda(eigenval, lambd, SAVE_PATH, N_SRC)
-plot_k_pot(k_pot_list, SAVE_PATH, N_SRC)
+if __name__ == '__main__':
+    HOME = expanduser('~')
+    DAY = datetime.datetime.now()
+    DAY = DAY.strftime('%Y%m%d')
+    TIMESTR = time.strftime("%H%M%S")
+    SAVE_PATH = HOME + "/kCSD_results/" + DAY + '/' + TIMESTR
+    makemydir(SAVE_PATH)
+    save_source_code(SAVE_PATH, time.strftime("%Y%m%d-%H%M%S"))
+
+    CSD_PROFILE = CSD.gauss_1d_mono
+    CSD_SEED = 15
+    N_SRC = [2, 4, 8, 16, 32, 64, 128, 256, 512]
+    ELE_LIMS = [0.1, 0.9]  # range of electrodes space
+    TRUE_CSD_XLIMS = [0., 1.]
+    TOTAL_ELE = 10
+    OBJ, RMS, POINT_ERROR, eigenval, eigenvec = stability_M(CSD_PROFILE,
+                                                            CSD_SEED,
+                                                            N_SRC, ELE_LIMS,
+                                                            TRUE_CSD_XLIMS,
+                                                            TOTAL_ELE)
+    k_pot_list = []
+    k_interp_cross_list = []
+    lambdas = []
+    for index in range(len(OBJ)):
+        k_pot_list.append(OBJ[index].k_pot)
+        k_interp_cross_list.append(OBJ[index].k_interp_cross)
+        lambdas.append(OBJ[index].lambd)
+
+    plot_M(N_SRC, RMS, SAVE_PATH)
+    plot_eigenvalues(eigenval, SAVE_PATH, N_SRC)
+    plot_eigenvectors(eigenvec, SAVE_PATH, N_SRC)
+    plot_k_interp_cross(k_interp_cross_list, SAVE_PATH, N_SRC)
+    plot_k_interp_cross_v(k_interp_cross_list, eigenvec, SAVE_PATH, N_SRC)
+    plot_eigenvalue_lambda(eigenval, lambdas, SAVE_PATH, N_SRC)
+    plot_k_pot(k_pot_list, SAVE_PATH, N_SRC)
