2851e550c1b0234da799723342fbab8a7593c6e6
Chaitanya Chintaluri <ccluri@gmail.com>
2018-08-10 14:40:34 +0100

    libre draw 5.1.6.2 version for the odg and settings for the remaining figures

---
+++ figures/kCSD_properties/figure_properties.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({
+    
+    'xtick.labelsize': 20,
+    'xtick.major.size': 10,
+    'ytick.labelsize': 20,
+    'ytick.major.size': 10,
+    'font.size': 20,
+    'axes.labelsize': 20,
+    'axes.titlesize': 15,
+    'legend.fontsize': 14,
+    'figure.subplot.wspace': 0.4,
+    'figure.subplot.hspace': 0.4,
+    'figure.subplot.left': 0.1,
+})
+
+
