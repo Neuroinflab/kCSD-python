33578dd3e41509e74cfd854afaa80b1d81952d82
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2018-09-07 15:02:22 +0200

    Square electrode distribution, correct orientation

--- figures/sKCSD_paper/make_fig_8.py
+++ figures/sKCSD_paper/make_fig_8.py
@@ -11,13 +11,14 @@ import sKCSD_utils
 import run_LFP
 n_src = 512
 dt = 0.5
-tolerance = 5e-6
+tolerance = 10e-6
 cmap = plt.cm.bwr_r
+n = 100
 if __name__ == '__main__':
     fname_base = "Figure_8"
     fig_name = sKCSD_utils.make_fig_names(fname_base)
     print(fig_name)
-    tstop = 85
+    tstop = 850
     scale_factor = 1000**2
     scale_factor_LFP = 1000
     R = 16e-6/np.sqrt(2)
@@ -31,16 +32,17 @@ if __name__ == '__main__':
                              weight=0.01,
                              n_syn=1000,
                              simulate_what='oscillatory',
-                             electrode_distribution=3,
+                             electrode_distribution=1,
                              electrode_orientation=3,
-                             xmin=-300,
+                             xmin=-400,
                              xmax=400,
-                             ymin=-600,
+                             ymin=-400,
                              ymax=400,
                              triside=45,
                              colnb=colnb,
                              rownb=rownb,
                              dt=dt)
+    c.save_for_R_kernel()
     data_dir = c.return_paths_skCSD_python()
     data = utils.LoadData(data_dir)
     ele_pos = data.ele_pos/scale_factor
@@ -56,32 +58,32 @@ if __name__ == '__main__':
     somav = np.loadtxt(os.path.join(data_dir,
                                            'somav.txt'))
     time = np.linspace(0, tstop, len(somav))
-    t0 = np.argmax(somav)
+    t0 = np.argmax(somav[int(400./dt):int(600./dt)])+int(400./dt)
  
     vmax, vmin = pl.get_min_max(ground_truth)
     
-    
-    cell_itself = sKCSDcell(morphology, ele_pos, n_src, tolerance=tolerance)
+    print(tolerance)
+    cell_itself = sKCSDcell(morphology, ele_pos, n_src, tolerance=tolerance, xmin=-650e-6, xmax=650e-6, ymin=-650e-6, ymax=650e-6)
     cell_itself.distribute_srcs_3D_morph()
     ground_truth_3D = cell_itself.transform_to_3D(ground_truth,
                                                   what="morpho")
     vmax, vmin = pl.get_min_max(ground_truth_3D[:, :, :, t0])
     morpho, extent = cell_itself.draw_cell2D(axis=2)
-    extent = [-600, 400, -400, 400]
-    for l in [1]:#, 1e-1, 1e-2, 1e-3, 1e-4]:
-        for R in [16e-6/np.sqrt(2), 8e-6/np.sqrt(2)]:#, 32e-6/np.sqrt(2)]:
+   
+    for l in [.1]:
+        for R in [16e-6/np.sqrt(2)]:#, 32e-6/np.sqrt(2)]:
             print(l/(2*(2*np.pi)**3*R**2*n_src))
             lambd = l/(2*(2*np.pi)**3*R**2*n_src)
-            fig = plt.figure()
+            fig = plt.figure(figsize=(8, 20))
             ax1 = plt.subplot2grid((3, 2), (0, 0), colspan=2)
         
             ax1.plot(time, somav)
             ax1.set_xlabel('time (ms)')
             ax1.set_ylabel('Vm (mV)')
-            ax2 = plt.subplot2grid((3, 2), (1, 0))
-            ax3 = plt.subplot2grid((3, 2), (1, 1))
-            ax4 = plt.subplot2grid((3, 2), (2, 0))
-            ax5 = plt.subplot2grid((3, 2), (2, 1))
+            ax2 = plt.subplot2grid((4, 2), (2, 0))
+            ax3 = plt.subplot2grid((4, 2), (2, 1))
+            ax4 = plt.subplot2grid((4, 2), (3, 0))
+            ax5 = plt.subplot2grid((4, 2), (3, 1))
             
             ker = sKCSD(ele_pos,
                         data.LFP,
@@ -90,13 +92,11 @@ if __name__ == '__main__':
                         src_type='gauss',
                         lambd=lambd,
                         R_init=R,
-                        dist_table_density=250,
+                        dist_table_density=100,
                         tolerance=tolerance,
                         exact=True)
- 
     
-    
-            skcsd = ker.values()
+            
             path = '%s_lambda_%f_R_%f' % (fname_base, l, R)
             if sys.version_info < (3, 0):
                 path = os.path.join(path, "preprocessed_data/Python_2")
@@ -105,9 +105,18 @@ if __name__ == '__main__':
             if not os.path.exists(path):
                 print("Creating", path)
                 os.makedirs(path)
-            # skcsd, pot, cell_obj = utils.load_sim(path)
-            utils.save_sim(path, ker)
-            est_skcsd = cell_itself.transform_to_3D(skcsd)
+            try:
+                utils.save_sim(path, ker)
+            except NameError:
+                pass
+            try:
+                skcsd = ker.values()
+            except NameError:
+                skcsd, pot, cell_obj = utils.load_sim(path)
+                skcsd = cell_itself.transform_to_3D(skcsd)
+                print(skcsd.shape)
+
+            est_skcsd = skcsd
             ax4.imshow(morpho,
                        origin='lower',
                        aspect='auto',
@@ -122,6 +131,10 @@ if __name__ == '__main__':
                              extent=extent,
                              cmap=cmap,alpha=.5)
             
+            ax2.set_yticklabels([])
+            ax3.set_yticklabels([])
+            ax2.set_xticklabels([])
+            ax3.set_xticklabels([])
 
             ax5.imshow(morpho,
                        origin='lower',
@@ -144,10 +157,10 @@ if __name__ == '__main__':
             ymax = cell_itself.ymax
             zmin = cell_itself.zmin
             zmax = cell_itself.zmax
-            gdx = (xmax-xmin)/100
-            gdy = (ymax-ymin)/2
-            gdz = (zmax-zmin)/200
-            
+            gdx = (xmax-xmin)/50
+            gdy = (ymax-ymin)/50
+            gdz = (zmax-zmin)/5
+            print(xmin, xmax, ymin, ymax)
             kcsd = KCSD3D(ele_pos,
                           data.LFP,
                           n_src_init=n_src,
@@ -181,7 +194,6 @@ if __name__ == '__main__':
             cax = ax2.imshow(kcsd_pot[:, :, :, t0].sum(axis=2),
                              origin='lower',
                              aspect='auto',
-                             interpolation='none',
                              vmin=vmin,
                              vmax=vmax,
                              extent=extent,
@@ -189,7 +201,6 @@ if __name__ == '__main__':
             cax = ax3.imshow(kcsd_csd[:, :, :, t0].sum(axis=2),
                              origin='lower',
                              aspect='auto',
-                             interpolation='none',
                              vmin=vmin,
                              vmax=vmax,
                              extent=extent,
@@ -206,12 +217,12 @@ if __name__ == '__main__':
                 text = ax5.text(pos_x, pos_y, '*',
                                 ha="center", va="center", color="k")
 
-            ax1.set_title('Time %d', int(t0))
+            ax1.set_title('Time %d' % t0)
             ax2.set_title('Potential')
-            ax1.set_title('kCSD')
-            ax1.set_title('Ground truth')
-            ax1.set_title('skCSD')
-            fig.savefig(fig_name+'%_f_%f.png'%(R, l),
+            ax3.set_title('kCSD')
+            ax4.set_title('Ground truth')
+            ax5.set_title('skCSD')
+            fig.savefig(fig_name+'.png',
                 bbox_inches='tight',
                 transparent=True,
                 pad_inches=0.1)
