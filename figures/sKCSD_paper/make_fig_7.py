f1f73f05df61103d4a472b9f250b6f9cb40b02d0
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2018-09-07 15:01:28 +0200

    Scale lambdas

--- figures/sKCSD_paper/make_fig_7.py
+++ figures/sKCSD_paper/make_fig_7.py
@@ -7,38 +7,36 @@ from kcsd import sKCSD
 import kcsd.utility_functions as utils
 import kcsd.validation.plotting_functions as pl
 import sKCSD_utils
+dt = 0.5
 if __name__ == '__main__':
     fname_base = "Figure_7"
     fig_name = sKCSD_utils.make_fig_names(fname_base + '.png')
     tstop = 70
     scale_factor = 1000**2
     scale_factor_LFP = 1000
-    R_inits = np.array([(2**(i - .5))/scale_factor for i in range(4, 7)])
-    lambdas = np.array([(10**(-i))for i in range(4, -4, -1)])
+    R_inits = np.array([(2**(i - .5))/scale_factor for i in range(3, 7)])
+    lambdas = np.array([(10**(-i))for i in range(5, 0, -1)])
     n_srcs = np.array([32, 64, 128, 512, 1024])
-    x_ticklabels = [2**i for i in range(3, 9)]
+    x_ticklabels = [2**i for i in range(3, 7)]
     y_ticklabels = [str(lambd) for lambd in lambdas]
     colnb = 4
     rownb = 4
-    fname = '%s_rows_%d' % (fname_base, rownb)
-    if sys.version_info < (3, 0):
-        c = sKCSD_utils.simulate(fname,
-                                 morphology=2,
-                                 colnb=colnb,
-                                 rownb=rownb,
-                                 xmin=-200,
-                                 xmax=600,
-                                 ymin=-200,
-                                 ymax=200,
-                                 tstop=tstop,
-                                 seed=1988,
-                                 weight=0.04,
-                                 n_syn=100,
-                                 simulate_what='symmetric')
-        new_path = c.return_paths_skCSD_python()
-    else:
-        new_path = os.path.join('simulation', fname)
-    data = utils.LoadData(new_path)
+    c = sKCSD_utils.simulate(fname_base,
+                             morphology=2,
+                             colnb=colnb,
+                             rownb=rownb,
+                             xmin=0,
+                             xmax=500,
+                             ymin=-100,
+                             ymax=100,
+                             tstop=tstop,
+                             seed=1988,
+                             weight=0.04,
+                             n_syn=100,
+                             electrode_orientation=2,
+                             simulate_what='symmetric',
+                             dt=dt)
+    data = utils.LoadData(c.return_paths_skCSD_python())
     ele_pos = data.ele_pos/scale_factor
     pots = data.LFP/scale_factor_LFP
     morphology = data.morphology
@@ -53,20 +51,21 @@ if __name__ == '__main__':
     for i, n_src in enumerate(n_srcs):
         for j, l in enumerate(lambdas):
             for k, R in enumerate(R_inits):
-                lambd = l
+                lambd = l/(16*np.pi**3*R**2*n_src)
                 ker = sKCSD(ele_pos,
                             pots,
                             morphology,
                             n_src_init=n_src,
-                            src_type='gauss_lim',
+                            src_type='gauss',
                             lambd=lambd,
                             R_init=R,
-                            skmonaco_available=False)
+                            exact=True)
                 est_skcsd = ker.values(estimate='CSD',
                                        transformation='segments')
+          
                 outs[i, j, k] = sKCSD_utils.L1_error(ground_truth,
                                                      est_skcsd)
-                print(outs[i, j, k])
+                print(outs[i, j, k], est_skcsd.min(), est_skcsd.max(), ground_truth.min(), ground_truth.max(), n_src, l, R)
     fig, ax = plt.subplots(1, 4, sharey=True)
     vmax = outs.max()
     vmin = outs.min()
