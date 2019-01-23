f3e2ec4fff8d4ec89a281a69bd932e70707cae25
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-01-23 19:45:21 +0100

    Remove l-curve from the figure

--- figures/kCSD_properties/skcsd_and_l_curve_complex_morphology.py
+++ figures/kCSD_properties/skcsd_and_l_curve_complex_morphology.py
@@ -11,9 +11,9 @@ import sKCSD_utils
 import matplotlib.gridspec as gridspec
 
 
-n_src = 512
-R = 16e-6
-lambd = 0.1#/(16*np.pi**3*R**2*n_src)
+n_src = 1024
+R = 32e-6
+lambd = 0.0001#/(16*np.pi**3*R**2*n_src)
 
 
 if __name__ == '__main__':
@@ -23,13 +23,12 @@ if __name__ == '__main__':
     scaling_factor_LFP = 1000
     R_inits = [2**(i-0.5)*1e-6 for i in range(3, 8)]
     lambdas = [10**(-i) for i in range(5)]
-    electrode_number = [10, 20, 30]
-    colnb = 20
-    data_dir = []
-    for rownb in electrode_number:
-        fname = "Figure_3_complex"
-        c = sKCSD_utils.simulate(fname_base,
-                             morphology=6,
+    rownb = 10
+    colnb = 10
+
+    fname = "Figure_3_complex"
+    c = sKCSD_utils.simulate(fname_base,
+                             morphology=7,
                              tstop=tstop,
                              seed=1988,
                              weight=0.04,
@@ -43,12 +42,12 @@ if __name__ == '__main__':
                              ymax=400,
                              colnb=colnb,
                              rownb=rownb,
-                                 dt=0.5)
+                             dt=0.5)
                                  
-        data_dir.append(c.return_paths_skCSD_python())
-    seglen = np.loadtxt(os.path.join(data_dir[0], 'seglength'))
+    data_dir = c.return_paths_skCSD_python()
+    seglen = np.loadtxt(os.path.join(data_dir, 'seglength'))
     n_seg = len(seglen)
-    ground_truth = np.loadtxt(os.path.join(data_dir[0], 'membcurr'))
+    ground_truth = np.loadtxt(os.path.join(data_dir, 'membcurr'))
     ground_truth = ground_truth/seglen[:, None]*1e-3
     gvmax, gvmin = pl.get_min_max(ground_truth)
     data_paths = []
@@ -76,56 +75,48 @@ if __name__ == '__main__':
     ax_gt.set_ylabel('#segment')
     new_fname = fname_base + '.png'
     fig_name = sKCSD_utils.make_fig_names(new_fname)
-    for i, datd in enumerate(data_dir):
-        print(datd)
-        data = utils.LoadData(datd)
-        ele_pos = data.ele_pos/scaling_factor
-        data.LFP = data.LFP/scaling_factor_LFP
-        morphology = data.morphology
-        morphology[:, 2:6] = morphology[:, 2:6]/scaling_factor
-        k = sKCSD(ele_pos,
-                  data.LFP,
-                  morphology,
-                  n_src_init=n_src,
-                  src_type='gauss',
-                  lambd=lambd,
-                  exact=True,
-                  R_init=R,
-                  sigma=0.3)
-        csd = k.values(transformation='segments')
-        print(csd.shape)
-        print(csd.max(), csd.min())
-        cax = ax[i].imshow(csd,
-                           extent=[0, tstop, 1, csd.shape[0]],
-                           origin='lower',
-                           aspect='auto',
-                           cmap='seismic_r',
-                           vmax=gvmax,
-                           vmin=gvmin)
-        ax[3+i].set_title(electrode_number[i])
-        #k.L_curve(lambdas=np.array(lambdas), Rs=np.array(R_inits))
-        #csd_Lcurve = k.values(transformation=None)
-        
-        # #Rcv, lambdacv = k.cross_validate(lambdas=np.array(lambdas), Rs=np.array(R_inits))
-       
-        # cax = ax[3+i].imshow(csd_Lcurve,
-        #                       extent=[0, tstop, 1, csd_Lcurve.shape[0]],
-        #                       origin='lower',
-        #                       aspect='auto',
-        #                       cmap='seismic_r',
-        #                       vmax=gvmax,
-        #                       vmin=gvmin)
-        ax[i].set_title('%d electrodes' % electrode_number[i])
-        ax[i].set_xticklabels([])
-        ax[i+3].set_xlabel('time (s)')
+    data = utils.LoadData(data_dir)
+    ele_pos = data.ele_pos/scaling_factor
+    data.LFP = data.LFP/scaling_factor_LFP
+    morphology = data.morphology
+    morphology[:, 2:6] = morphology[:, 2:6]/scaling_factor
+    k = sKCSD(ele_pos,
+              data.LFP,
+              morphology,
+              n_src_init=n_src,
+              src_type='gauss',
+              lambd=lambd,
+              exact=True,
+              R_init=R,
+              sigma=0.3)
+    path = os.path.join(data_dir, 'lambda_%f_R_%f_n_src_%d' % (lambd, R, n_src))
+    if sys.version_info < (3, 0):
+        path = os.path.join(path, "preprocessed_data/Python_2")
+    else:
+        path = os.path.join(path, "preprocessed_data/Python_3")
+    if not os.path.exists(path):
+        print("Creating", path)
+        os.makedirs(path)
+    try:
+        utils.save_sim(path, k)
+    except NameError:
+        pass
+    skcsd, pot, cell_obj = utils.load_sim(path)
+    csd = cell_obj.transform_to_segments(skcsd)
+    print(csd.shape)
+    print(csd.max(), csd.min())
+    cax = ax[1].imshow(csd,
+                       extent=[0, tstop, 1, csd.shape[0]],
+                       origin='lower',
+                       aspect='auto',
+                       cmap='seismic_r',
+                       vmax=gvmax,
+                       vmin=gvmin)
+    ax[1].set_title('10 x 10')
+    ax[1].set_title('%d electrodes' % colnb*rownb)
+    ax[1].set_xticklabels([])
+    ax[1].set_xlabel('time (s)')
   
-        if i:
-            ax[i].set_yticklabels([])
-            ax[i+3].set_yticklabels([])
-        else:
-            ax[i].set_ylabel('#segment')
-            ax[i+3].set_ylabel('#segment')
-            
     
     plt.savefig(fig_name,
                 bbox_inches='tight',
