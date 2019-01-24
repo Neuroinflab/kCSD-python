c2a1c21aa93934df8d56fa79e61b9ac4d68c873b
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-01-24 12:47:59 +0100

    Divide code into functions

--- figures/kCSD_properties/skcsd_and_l_curve_complex_morphology.py
+++ figures/kCSD_properties/skcsd_and_l_curve_complex_morphology.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-from kcsd import sKCSD
+from kcsd import sKCSD, sKCSDcell
 import kcsd.utility_functions as utils
 import kcsd.validation.plotting_functions as pl
 sys.path.insert(1, os.path.join(sys.path[0], '../sKCSD_paper'))
@@ -13,20 +13,56 @@ import matplotlib.gridspec as gridspec
 
 n_src = 1024
 R = 32e-6
-lambd = 0.0001#/(16*np.pi**3*R**2*n_src)
+lambd = 0.1
+fname = "Figure_complex"
+scaling_factor = 1000**2
+scaling_factor_LFP = 1000
 
 
-if __name__ == '__main__':
-    fname_base = "Figure_complex"
+def make_larger_cell(data, n_sources=n_src):
+    if data.ele_pos[:, 0].max() > data.morphology[:, 2].max():
+        xmax = data.ele_pos[:, 0].max() + 50e-6
+    else:
+        xmax = data.morphology[:, 2].max() + 50e-6
+    if data.ele_pos[:, 0].min() < data.morphology[:, 2].min():
+        xmin = data.ele_pos[:, 0].min() - 50e-6
+    else:
+        xmin = data.morphology[:, 2].min() - 50e-6
+    
+    if data.ele_pos[:, 1].max() > data.morphology[:, 3].max():
+        ymax = data.ele_pos[:, 1].max() + 50e-6
+    else:
+        ymax = data.morphology[:, 3].max() + 50e-6
+    if data.ele_pos[:, 1].min() < data.morphology[:, 3].min():
+        ymin = data.ele_pos[:, 1].min() - 50e-6
+    else:
+        ymin = data.morphology[:, 3].min() - 50e-6
+
+    if data.ele_pos[:, 2].max() > data.morphology[:, 4].max():
+        zmax = data.ele_pos[:, 2].max() + 50e-6
+    else:
+        zmax = data.morphology[:, 4].max() + 50e-6
+    if data.ele_pos[:, 2].min() < data.morphology[:, 4].min():
+        zmin = data.ele_pos[:, 2].min() - 50e-6
+    else:
+        zmin = data.morphology[:, 4].min() - 50e-6
+   
+    return sKCSDcell(data.morphology, data.ele_pos, n_sources, xmin=xmin, xmax=xmax, zmin=zmin, zmax=zmax, ymin=ymin, ymax=ymax, tolerance=5e-6)
+
+def make_figure():
+    fig = plt.figure(figsize=(20, 6))
+    gs = gridspec.GridSpec(2, 5, figure=fig)
+    ax_morpho = plt.subplot(gs[0,0])
+    ax = []
+    for i in range(2):
+        for j in range(2, 5):
+            ax.append(plt.subplot(gs[i, j]))
+    return fig, ax_morpho, ax
+
+def simulate():
     tstop = 75
-    scaling_factor = 1000**2
-    scaling_factor_LFP = 1000
-    R_inits = [2**(i-0.5)*1e-6 for i in range(3, 8)]
-    lambdas = [10**(-i) for i in range(5)]
     rownb = 10
     colnb = 10
-
-    fname = "Figure_3_complex"
     c = sKCSD_utils.simulate(fname_base,
                              morphology=7,
                              tstop=tstop,
@@ -43,52 +79,56 @@ if __name__ == '__main__':
                              colnb=colnb,
                              rownb=rownb,
                              dt=0.5)
-                                 
-    data_dir = c.return_paths_skCSD_python()
-    seglen = np.loadtxt(os.path.join(data_dir, 'seglength'))
+    return c.return_paths_skCSD_python()
+
+def read_in_data(ddir):
+    seglen = np.loadtxt(os.path.join(ddir, 'seglength'))
     n_seg = len(seglen)
-    ground_truth = np.loadtxt(os.path.join(data_dir, 'membcurr'))
+    ground_truth = np.loadtxt(os.path.join(ddir, 'membcurr'))
+    time = np.loadtxt(os.path.join(ddir, 'tvec.txt'))
     ground_truth = ground_truth/seglen[:, None]*1e-3
-    gvmax, gvmin = pl.get_min_max(ground_truth)
-    data_paths = []
-    print(ground_truth.min(), ground_truth.max())
-    fig =   plt.figure(figsize=(20, 6))
+    Data = utils.LoadData(ddir)
+    Data.ele_pos = Data.ele_pos/scaling_factor
+    Data.LFP = Data.LFP/scaling_factor_LFP
+    Data.morphology[:, 2:6] = Data.morphology[:, 2:6]/scaling_factor
+    return ground_truth, Data, time
+
+if __name__ == '__main__':
+    fname_base = "Figure_complex"
+    data_dir = simulate()
+    fig, ax_morpho, ax = make_figure()
+    ground_truth, data, time = read_in_data(data_dir)
+    gvmax, gvmin = pl.get_min_max(ground_truth)        
+    cax = ax[0].imshow(ground_truth,
+                       extent=[0, time[-1], 1, ground_truth.shape[0]],
+                       origin='lower',
+                       aspect='auto',
+                       cmap='seismic_r',
+                       vmax=gvmax,
+                       vmin=gvmin)
+    ax_morpho.set_title('Cell morphology and morphology loop')
+    ax_morpho.set_xlabel('')
+    ax_morpho.set_ylabel('')
+
     
-    gs = gridspec.GridSpec(2, 5, figure=fig)
-
-    ax_gt = plt.subplot(gs[0,0])
-    ax = []
-    for i in range(2):
-        for j in range(2, 5):
-            ax.append(plt.subplot(gs[i, j]))
-
-            
-    cax = ax_gt.imshow(ground_truth,
-                          extent=[0, tstop, 1, ground_truth.shape[0]],
-                          origin='lower',
-                          aspect='auto',
-                          cmap='seismic_r',
-                          vmax=gvmax,
-                          vmin=gvmin)
-    ax_gt.set_title('Ground truth')
-    ax_gt.set_xlabel('time (s)')
-    ax_gt.set_ylabel('#segment')
-    new_fname = fname_base + '.png'
+    new_fname = fname + '.png'
     fig_name = sKCSD_utils.make_fig_names(new_fname)
-    data = utils.LoadData(data_dir)
-    ele_pos = data.ele_pos/scaling_factor
-    data.LFP = data.LFP/scaling_factor_LFP
-    morphology = data.morphology
-    morphology[:, 2:6] = morphology[:, 2:6]/scaling_factor
-    k = sKCSD(ele_pos,
-              data.LFP,
-              morphology,
-              n_src_init=n_src,
-              src_type='gauss',
-              lambd=lambd,
-              exact=True,
-              R_init=R,
-              sigma=0.3)
+    cell_itself = make_larger_cell(data, n_src)
+    morphology, extent = cell_itself.draw_cell2D()
+    print(morphology.shape)
+    ax_morpho.imshow(morphology,
+                     origin="lower",
+                     interpolation="none",
+                     extent=extent)
+    # k = sKCSD(data.ele_pos,
+    #           data.LFP,
+    #           data.morphology,
+    #           n_src_init=n_src,
+    #           src_type='gauss',
+    #           lambd=lambd,
+    #           exact=True,
+    #           R_init=R,
+    #           sigma=0.3)
     path = os.path.join(data_dir, 'lambda_%f_R_%f_n_src_%d' % (lambd, R, n_src))
     if sys.version_info < (3, 0):
         path = os.path.join(path, "preprocessed_data/Python_2")
@@ -106,14 +146,13 @@ if __name__ == '__main__':
     print(csd.shape)
     print(csd.max(), csd.min())
     cax = ax[1].imshow(csd,
-                       extent=[0, tstop, 1, csd.shape[0]],
+                       extent=[0, time[-1], 1, csd.shape[0]],
                        origin='lower',
                        aspect='auto',
                        cmap='seismic_r',
                        vmax=gvmax,
                        vmin=gvmin)
     ax[1].set_title('10 x 10')
-    ax[1].set_title('%d electrodes' % colnb*rownb)
     ax[1].set_xticklabels([])
     ax[1].set_xlabel('time (s)')
   
@@ -121,9 +160,11 @@ if __name__ == '__main__':
     plt.savefig(fig_name,
                 bbox_inches='tight',
                 transparent=True,
-                pad_inches=0.1)
+                pad_inches=0.1,
+                dpi=600)
     plt.savefig(fig_name[:-4]+'.svg',
                 bbox_inches='tight',
                 transparent=True,
-                pad_inches=0.1)
+                pad_inches=0.1,
+                dpi=600)
     plt.show()
