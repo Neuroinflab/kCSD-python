8edcab70b9b49c4c701a5b2f85a3f339509931e2
m-kowalska <m.kowalska@nencki.gov.pl>
2019-03-07 14:33:04 +0100

    Fixed indent

--- kcsd/validation/ValidateKCSD.py
+++ kcsd/validation/ValidateKCSD.py
@@ -527,13 +527,13 @@ class ValidateKCSD(object):
             Normalized error of reconstruction calculated separetly at every
             point of estimation space.
         """
-            true_csd_r = true_csd.reshape(true_csd.size, 1)
-            est_csd_r = est_csd.reshape(est_csd.size, 1)
-            epsilon = np.linalg.norm(true_csd_r)/np.max(abs(true_csd_r))
-            err_r = abs(est_csd_r/(np.linalg.norm(est_csd_r)) -
-                      true_csd_r/(np.linalg.norm(true_csd_r)))
-            err_r *= epsilon
-            point_error = err_r.reshape(true_csd.shape)
+        true_csd_r = true_csd.reshape(true_csd.size, 1)
+        est_csd_r = est_csd.reshape(est_csd.size, 1)
+        epsilon = np.linalg.norm(true_csd_r)/np.max(abs(true_csd_r))
+        err_r = abs(est_csd_r/(np.linalg.norm(est_csd_r)) -
+                  true_csd_r/(np.linalg.norm(true_csd_r)))
+        err_r *= epsilon
+        point_error = err_r.reshape(true_csd.shape)
         return point_error
 
     def calculate_rdm(self, true_csd, est_csd):
