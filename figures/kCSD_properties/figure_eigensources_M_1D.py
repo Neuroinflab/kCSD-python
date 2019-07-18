9a24ecad5ec5a6785228fbf6f6ffccf07d5f1655
Marta Kowalska <m.kowalska@nencki.gov.pl>
2019-07-18 12:50:32 +0200

    Scripts producing figures for kCSD paper (#104)
    * corrected algorithm for distribution of basis sources in 2D
    
    * updated documentation: validation part
    
    * Added script that produces plot for unitary potential propagation
    
    * code refactoring
    
    * Renamed script that produces reliability map plot
    
    * Renamed script that produces example of kCSD reconstruction with reliability map in 2D
    
    * Code and documentation refactoring
    
    * Code refactoring, changed scripts names
    
    * Common scale on the plot
    
    * Common scale on the plot, error formulation as in the paper
    
    * Fixed typo
    
    * Removed redundant variables.
    
    * Fixed round problem - generating electrodes in 3D
    
    * Fixed potential unit
    
    * Electrodes markers on the X-axis

--- figures/kCSD_properties/figure_eigensources_M_1D.py
+++ figures/kCSD_properties/figure_eigensources_M_1D.py
@@ -4,16 +4,14 @@
 import os
 from os.path import expanduser
 import numpy as np
+from numpy.linalg import LinAlgError
 import matplotlib.pyplot as plt
 from figure_properties import *
 import matplotlib.gridspec as gridspec
-from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
-from mpl_toolkits.axes_grid1.inset_locator import mark_inset
-from matplotlib.ticker import FuncFormatter
 import datetime
 import time
 
-from kcsd import SpectralStructure, KCSD1D
+from kcsd import KCSD1D
 import targeted_basis as tb
 
 __abs_file__ = os.path.abspath(__file__)
@@ -23,38 +21,24 @@ def _html(r, g, b):
     return "#{:02X}{:02X}{:02X}".format(r, g, b)
 
 
-def stability_M(csd_profile, n_src, ele_lims, true_csd_xlims,
-                total_ele, ele_pos, pots,
-                method='cross-validation', Rs=None, lambdas=None):
+def stability_M(n_src, total_ele, ele_pos, pots, R_init=0.23):
     """
     Investigates stability of reconstruction for different number of basis
     sources
 
     Parameters
     ----------
-    csd_profile: function
-        Function to produce csd profile.
     n_src: int
         Number of basis sources.
-    ele_lims: list
-        Boundaries for electrodes placement.
-    true_csd_xlims: list
-        Boundaries for ground truth space.
     total_ele: int
         Number of electrodes.
     ele_pos: numpy array
         Electrodes positions.
     pots: numpy array
         Values of potentials at ele_pos.
-    method: string
-        Determines the method of regularization.
-        Default: cross-validation.
-    Rs: numpy 1D array
-        Basis source parameter for crossvalidation.
-        Default: None.
-    lambdas: numpy 1D array
-        Regularization parameter for crossvalidation.
-        Default: None.
+    R_init: float
+        Initial value of R parameter - width of basis source
+        Default: 0.23.
 
     Returns
     -------
@@ -70,14 +54,19 @@ def stability_M(csd_profile, n_src, ele_lims, true_csd_xlims,
     for i, value in enumerate(n_src):
         pots = pots.reshape((len(ele_pos), 1))
         obj = KCSD1D(ele_pos, pots, src_type='gauss', sigma=0.3, h=0.25,
-                     gdx=0.01, n_src_init=n_src[i], ext_x=0, xmin=0, xmax=1)
-        if method == 'cross-validation':
-            obj.cross_validate(Rs=Rs, lambdas=lambdas)
-        elif method == 'L-curve':
-            obj.L_curve(Rs=Rs, lambdas=lambdas)
-        ss = SpectralStructure(obj)
-        eigenvectors[i], eigenvalues[i] = ss.evd()
-
+                     gdx=0.01, n_src_init=n_src[i], ext_x=0, xmin=0, xmax=1,
+                     R_init=R_init)
+        try:
+            eigenvalue, eigenvector = np.linalg.eigh(obj.k_pot +
+                                                     obj.lambd *
+                                                     np.identity
+                                                     (obj.k_pot.shape[0]))
+        except LinAlgError:
+            raise LinAlgError('EVD is failing - try moving the electrodes'
+                              'slightly')
+        idx = eigenvalue.argsort()[::-1]
+        eigenvalues[i] = eigenvalue[idx]
+        eigenvectors[i] = eigenvector[:, idx]
         obj_all.append(obj)
     return obj_all, eigenvalues, eigenvectors
 
@@ -112,8 +101,7 @@ def set_axis(ax, x, y, letter=None):
 
 
 def generate_figure(csd_profile, R, MU, true_csd_xlims, total_ele, ele_lims,
-                    save_path, method='cross-validation', Rs=None,
-                    lambdas=None, noise=0):
+                    save_path, noise=0, R_init=0.23):
     """
     Generates figure for spectral structure decomposition.
 
@@ -135,18 +123,12 @@ def generate_figure(csd_profile, R, MU, true_csd_xlims, total_ele, ele_lims,
         Electrodes limits.
     save_path: string
         Directory.
-    method: string
-        Determines the method of regularization.
-        Default: cross-validation.
-    Rs: numpy 1D array
-        Basis source parameter for crossvalidation.
-        Default: None.
-    lambdas: numpy 1D array
-        Regularization parameter for crossvalidation.
-        Default: None.
     noise: float
         Determines the level of noise in the data.
         Default: 0.
+    R_init: float
+        Initial value of R parameter - width of basis source
+        Default: 0.23.
 
     Returns
     -------
@@ -159,18 +141,16 @@ def generate_figure(csd_profile, R, MU, true_csd_xlims, total_ele, ele_lims,
                                                             noise=noise)
 
     n_src_M = [2, 4, 8, 16, 32, 64, 128, 256, 512]
-    OBJ_M, eigenval_M, eigenvec_M = stability_M(csd_profile, n_src_M,
-                                                ele_lims, true_csd_xlims,
+    OBJ_M, eigenval_M, eigenvec_M = stability_M(n_src_M,
                                                 total_ele, ele_pos, pots,
-                                                method=method, Rs=Rs,
-                                                lambdas=lambdas)
+                                                R_init=R_init)
 
-    plt_cord = [(3, 0), (3, 2), (3, 4),
+    plt_cord = [(2, 0), (2, 2), (2, 4),
+                (3, 0), (3, 2), (3, 4),
                 (4, 0), (4, 2), (4, 4),
-                (5, 0), (5, 2), (5, 4),
-                (6, 0), (6, 2), (6, 4)]
+                (5, 0), (5, 2), (5, 4)]
 
-    letters = ['B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M']
+    letters = ['C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N']
 
     BLACK = _html(0, 0, 0)
     ORANGE = _html(230, 159, 0)
@@ -182,52 +162,40 @@ def generate_figure(csd_profile, R, MU, true_csd_xlims, total_ele, ele_lims,
     PURPLE = _html(204, 121, 167)
     colors = [BLUE, ORANGE, GREEN, PURPLE, VERMILION, SKY_BLUE, YELLOW, BLACK]
 
-    fig = plt.figure(figsize=(21, 18))
+    fig = plt.figure(figsize=(18, 16))
 #    heights = [1, 1, 1, 0.2, 1, 1, 1, 1]
-    heights = [2, 2, 0.3, 1, 1, 1, 1]
+    heights = [4, 0.3, 1, 1, 1, 1]
     markers = ['^', '.', '*', 'x', ',']
 #    linestyles = [':', '--', '-.', '-']
     linestyles = ['-', '-', '-', '-']
     src_idx = [0, 2, 3, 8]
 
-    gs = gridspec.GridSpec(7, 6, height_ratios=heights, hspace=0.5, wspace=0.6)
+    gs = gridspec.GridSpec(6, 6, height_ratios=heights, hspace=0.3, wspace=0.6)
 
-    ax = fig.add_subplot(gs[0:2, :])
+    ax = fig.add_subplot(gs[0, :3])
     for indx, i in enumerate(src_idx):
         ax.plot(np.arange(1, total_ele + 1), eigenval_M[i],
                 linestyle=linestyles[indx], color=colors[indx],
                 marker=markers[indx], label='M='+str(n_src_M[i]),
                 markersize=10)
-#    ax.set_title(' ', fontsize=12)
     ht, lh = ax.get_legend_handles_labels()
     set_axis(ax, -0.05, 1.05, letter='A')
-#    ax.legend(loc='lower left')
     ax.set_xlabel('Number of components')
     ax.set_ylabel('Eigenvalues')
     ax.set_yscale('log')
     ax.set_ylim([1e-6, 1])
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
-#    ax = fig.add_subplot(gs[0:3, 3:])
-    axins = zoomed_inset_axes(ax, 7., loc=3, borderpad=4)
-    for indx, i in enumerate(src_idx):
-        axins.plot(np.arange(1, total_ele + 1), eigenval_M[i],
-                   linestyle=linestyles[indx], color=colors[indx],
-                   marker=markers[indx], label='M='+str(n_src_M[i]),
-                   markersize=6)
-    axins.set_xlim([0.9, 1.1])
-    axins.set_ylim([0.2, 0.4])
-    axins.get_xaxis().set_visible(False)
-#    axins.spines['right'].set_visible(False)
-#    axins.spines['top'].set_visible(False)
-    mark_inset(ax, axins, loc1=2, loc2=1, fc="none", ec="0.5")
-#    cbaxes.plot(n_src_M, eigenval_M[:, 0], marker='s', color='k',
-#                markersize=5, linestyle=' ')
-#    ax.set_title(' ', fontsize=12)
-#     set_axis(ax, -0.05, 1.01, letter='B')
-#    ax.set_xlabel('Number of basis sources', fontsize=12)
-#    ax.set_xscale('log')
-#    ax.set_ylabel('Eigenvalues', fontsize=12)
+
+    ax = fig.add_subplot(gs[0, 3:])
+    ax.plot(n_src_M, eigenval_M[:, 0], marker='s', color='k', markersize=5,
+            linestyle=' ')
+    set_axis(ax, -0.05, 1.05, letter='B')
+    ax.set_xlabel('Number of basis sources')
+    ax.set_xscale('log')
+    ax.set_ylabel('Eigenvalues')
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
 
     for i in range(OBJ_M[0].k_interp_cross.shape[1]):
         ax = fig.add_subplot(gs[plt_cord[i][0],
@@ -237,11 +205,9 @@ def generate_figure(csd_profile, R, MU, true_csd_xlims, total_ele, ele_lims,
                     eigenvec_M[j, :, i]),
                     linestyle=linestyles[idx], color=colors[idx],
                     label='M='+str(n_src_M[j]), lw=2)
-            ax.set_title(r"$\tilde{K}*v_{{%(i)d}}$" % {'i': i+1})
-#            ax.locator_params(axis='y', nbins=3)
-
-#            ax.set_xlabel('Depth (mm)', fontsize=12)
-#            ax.set_ylabel('CSD (mA/mm)', fontsize=12)
+            ax.text(0.5, 1., r"$\tilde{K}\cdot{v_{{%(i)d}}}$" % {'i': i+1},
+                    horizontalalignment='center', transform=ax.transAxes,
+                    fontsize=20)
             set_axis(ax, -0.10, 1.1, letter=letters[i])
             if i < 9:
                 ax.get_xaxis().set_visible(False)
@@ -251,23 +217,12 @@ def generate_figure(csd_profile, R, MU, true_csd_xlims, total_ele, ele_lims,
             if i % 3 == 0:
                 ax.set_ylabel('CSD ($mA/mm$)')
                 ax.yaxis.set_label_coords(-0.18, 0.5)
-#            ax.yaxis.get_major_formatter().set_powerlimits((0, 1))
-#            ax.tick_params(direction='out', pad=10)
-#            ax.yaxis.get_major_formatter(FormatStrFormatter('%.2f'))
             ax.ticklabel_format(style='sci', axis='y', scilimits=((0.0, 0.0)))
             ax.spines['right'].set_visible(False)
             ax.spines['top'].set_visible(False)
-#     ht, lh = ax.get_legend_handles_labels()
-
-#     ax = fig.add_subplot(gs[3, :])
-#     ax.legend(ht, lh,  fancybox=False, shadow=False, ncol=len(src_idx),
-#               loc='upper center', frameon=False, bbox_to_anchor=(0.5, 0.0))
-#     ax.axis('off')
-
-#    plt.tight_layout()
     fig.legend(ht, lh, loc='lower center', ncol=5, frameon=False)
-    fig.savefig(os.path.join(save_path, 'vectors_' + method +
-                             '_noise_' + str(noise) + '.png'), dpi=300)
+    fig.savefig(os.path.join(save_path, 'vectors_' + '_noise_' +
+                             str(noise) + 'R0_2' + '.png'), dpi=300)
 
     plt.show()
 
@@ -287,6 +242,6 @@ if __name__ == '__main__':
     CSD_PROFILE = tb.csd_profile
     R = 0.2
     MU = 0.25
+    R_init = 0.2
     generate_figure(CSD_PROFILE, R, MU, TRUE_CSD_XLIMS, TOTAL_ELE, ELE_LIMS,
-                    SAVE_PATH, method='cross-validation',
-                    Rs=np.arange(0.1, 0.5, 0.05), noise=None)
+                    SAVE_PATH, noise=None, R_init=R_init)
--- kcsd/validation/VisibilityMap.py
+++ kcsd/validation/VisibilityMap.py
@@ -13,7 +13,6 @@ from matplotlib import gridspec
 
 from kcsd import ValidateKCSD1D, ValidateKCSD2D, ValidateKCSD3D
 from kcsd import csd_profile as CSD
-from kcsd import KCSD1D, KCSD2D, KCSD3D
 
 try:
     from joblib import Parallel, delayed
@@ -247,7 +246,6 @@ class VisibilityMap2D(ValidateKCSD2D):
                                               self.h, self.sigma,
                                               noise=noise,
                                               nr_broken_ele=nr_broken_ele)
-
         k, est_csd = self.do_kcsd(pots, ele_pos, method=method, Rs=Rs,
                                   lambdas=lambdas)
         test_csd = csd_profile([k.estm_x, k.estm_y], csd_seed)
@@ -349,9 +347,9 @@ class VisibilityMap2D(ValidateKCSD2D):
         mean_error = self.sigmoid_mean(point_error)
         plt.figure(figsize=(12, 7))
         ax1 = plt.subplot(111, aspect='equal')
-        levels = np.linspace(0, 1., 25)
+#        levels = np.linspace(0, 1., 25)
         im = ax1.contourf(x, y, mean_error, cmap='Greys')
-        plt.colorbar(im)#im, fraction=0.046, pad=0.06)
+        plt.colorbar(im)
         plt.scatter(ele_x, ele_y, 10, c='k')
         ax1.set_xlabel('Depth x [mm]')
         ax1.set_ylabel('Depth y [mm]')
@@ -551,23 +549,23 @@ if __name__ == '__main__':
     CSD_PROFILE = CSD.gauss_1d_mono
     ELE_LIMS = [0.1, 0.9]  # range of electrodes space
     TRUE_CSD_XLIMS = [0., 1.]
-#    k = VisibilityMap1D(total_ele=10, h=0.25, R_init=0.3, ele_lims=ELE_LIMS,
-#                        true_csd_xlims=TRUE_CSD_XLIMS, sigma=0.3,
-#                        src_type='gauss', n_src_init=100, ext_x=0.1)
-#    rms, point_error = k.calculate_error_map(CSD_PROFILE,
-#                                             Rs=np.arange(0.2, 0.5, 0.1))
-#    ele_pos = np.linspace(ELE_LIMS[0], ELE_LIMS[1], 32)
+    k = VisibilityMap1D(total_ele=10, h=0.25, R_init=0.3, ele_lims=ELE_LIMS,
+                        true_csd_xlims=TRUE_CSD_XLIMS, sigma=0.3,
+                        src_type='gauss', n_src_init=100, ext_x=0.1)
+    rms, point_error = k.calculate_error_map(CSD_PROFILE,
+                                             Rs=np.arange(0.2, 0.5, 0.1))
 
     print('Checking 2D')
-    ELE_LIMS = [0.05, 0.95]
+    ELE_LIMS = [0.118, 0.882]
     CSD_PROFILE = CSD.gauss_2d_large
-    a = VisibilityMap2D(total_ele=100, h=50., sigma=1., n_src_init=400,
+    a = VisibilityMap2D(total_ele=9, h=50., sigma=1., n_src_init=1000,
                         ele_lims=ELE_LIMS, true_csd_xlims=TRUE_CSD_XLIMS,
                         est_xres=0.01, est_yres=0.01)
-    rms, point_error = a.calculate_error_map(CSD_PROFILE, Rs=np.arange(0.05, 0.5, 0.05),
-                                             lambdas=np.array(0), n=70)
+    rms, point_error = a.calculate_error_map(CSD_PROFILE,
+                                             Rs=np.arange(0.05, 0.5, 0.05),
+                                             lambdas=np.array(0), n=2)
 
-#    print('Checking 3D')
-#    CSD_PROFILE = CSD.gauss_3d_small
-#    a = VisibilityMap3D(total_ele=64, h=50., sigma=1., n_src_init=729)
-#    a.calculate_error_map(CSD_PROFILE)
+    print('Checking 3D')
+    CSD_PROFILE = CSD.gauss_3d_small
+    a = VisibilityMap3D(total_ele=64, h=50., sigma=1., n_src_init=729)
+    a.calculate_error_map(CSD_PROFILE)
