3e5fe890a167304db87d4a43dd3fc717395ed0e8
wsredniawa <32837003+wsredniawa@users.noreply.github.com>
2020-07-10 10:51:03 +0200

    Update KCSD.py
    values() - loop is changed to matrix multiplication, kcsd works faster
    
    Bug in oKCSD classes fixed - place_basis() overwritten in classes

--- kcsd/KCSD.py
+++ kcsd/KCSD.py
@@ -86,8 +86,7 @@ class KCSD(CSD):
         Parameters
         ----------
         **kwargs
-            Same as those passed to initialize the Class
-
+            Same as those passed to initialize the Clas
         Raises
         ------
         TypeError
@@ -219,12 +218,9 @@ class KCSD(CSD):
             print('Invalid quantity to be measured, pass either CSD or POT')
         kernel = self.k_pot + self.lambd * np.identity(self.k_pot.shape[0])
         estimation = np.zeros((self.n_estm, self.n_time))
-        for t in range(self.n_time):
-            # C*(x) [Potworowski 2018 Eq 2.18]
-            # `inv(K) V` calculated by solving `K X = V` for `X`
-            estimation[:, t] = np.dot(estimation_table,
-                                      np.linalg.solve(kernel,
-                                                      self.pots[:, t]))
+        beta_new = np.dot(np.linalg.inv(kernel), self.pots)
+        estimation = np.dot(estimation_table, beta_new)
+        
         return self.process_estimate(estimation)
 
     def process_estimate(self, estimation):
@@ -1128,6 +1124,101 @@ class KCSD3D(KCSD):
         pot *= basis_func(dist, R)
         return pot
 
+class oKCSD1D(KCSD1D):
+    """oKCSD - The variant for the Kernel Current Source Density method that 
+    allows to reconstruct potential and CSD in given 1D space points.
+    """
+    def __init__(self, ele_pos, pots, **kwargs):
+        """Initialize oKCSD1D Class.
+
+        Parameters
+        ----------
+        ele_pos : numpy array
+            positions of electrodes
+        pots : numpy array
+            potentials measured by electrodes
+        **kwargs
+            configuration parameters, that may contain the following keys:
+            src_type : str
+                basis function type ('gauss', 'step', 'gauss_lim')
+                Defaults to 'gauss'
+            sigma : float
+                space conductance of the tissue in S/m
+                Defaults to 1 S/m
+            n_src_init : int
+                requested number of sources
+                Defaults to 1000
+            R_init : float
+                demanded thickness of the basis element
+                Defaults to 0.23
+            h : float
+                thickness of analyzed tissue slice
+                Defaults to 1.
+            own_est: numpy array
+                points coordinates of estimation places. If not given 
+                estimation places will be taken from own_src
+            own_src: numpy array
+                points coordinates of basis source centers 
+            lambd : float
+                regularization parameter for ridge regression
+                Defaults to 0.
+
+        Raises
+        ------
+        LinAlgError
+            Could not invert the matrix, try changing the ele_pos slightly
+        KeyError
+            Basis function (src_type) not implemented.
+            See basis_functions.py for available
+
+        """
+        self.own_src = kwargs.pop('own_src', np.array([]))
+        self.own_est = kwargs.pop('own_est', np.array([]))
+        if not self.own_est.any(): self.own_est = self.own_src
+        if not self.own_est.any() and not self.own_src.any():
+            raise KeyError('"own_src" is required argument to use oKCSD1D.' +
+                           'If you would like to reconstruct in default ' +
+                           'region of interest please use KCSD1D')
+        super(oKCSD1D, self).__init__(ele_pos, pots, **kwargs)
+        self.dim = 'own'
+
+    def estimate_at(self):
+        """Redefines locations where the estimation is wanted
+
+        Defines:
+        self.n_estm = self.estm_x.size
+        self.estm_x : Locations at which CSD is requested.
+
+        """
+        self.estm_x = self.own_est
+        self.src_x = self.own_src
+        self.n_estm = self.estm_x.size
+
+    def place_basis(self):
+        """Places basis sources of the defined type.
+
+        Checks if a given source_type is defined, if so then defines it
+        self.basis, This function gives locations of the basis sources,
+        Defines
+        source_type : basis_fuctions.basis_1D.keys()
+        self.R based on R_init
+        self.dist_max as maximum distance between electrode and basis
+        self.nsx = self.src_x.shape
+        self.src_x : Locations at which basis sources are placed.
+
+        """
+        source_type = self.src_type
+        try:
+            self.basis = basis.basis_1D[source_type]
+        except KeyError:
+            raise KeyError('Invalid source_type for basis! available are:',
+                           basis.basis_1D.keys())
+
+        self.R = self.R_init
+        self.src_x = self.own_src
+        self.n_src = self.src_x.size
+        self.nsx = self.src_x.shape
+    
 class oKCSD2D(KCSD2D):
     """oKCSD - The variant for the Kernel Current Source Density method that 
     allows to reconstruct potential and CSD in given 2D space points.
@@ -1197,6 +1288,31 @@ class oKCSD2D(KCSD2D):
         self.estm_x, self.estm_y = self.own_est
         self.src_x, self.src_y = self.own_src
         self.n_estm = self.estm_x.size
+   
+    def place_basis(self):
+        """Places basis sources of the defined type.
+
+        Checks if a given source_type is defined, if so then defines it
+        self.basis, This function gives locations of the basis sources,
+        Defines
+        source_type : basis_fuctions.basis_1D.keys()
+        self.R based on R_init
+        self.dist_max as maximum distance between electrode and basis
+        self.nsx = self.src_x.shape
+        self.src_x, self.src_y : Locations at which basis sources are placed.
+
+        """
+        source_type = self.src_type
+        try:
+            self.basis = basis.basis_1D[source_type]
+        except KeyError:
+            raise KeyError('Invalid source_type for basis! available are:',
+                           basis.basis_1D.keys())
+
+        self.R = self.R_init
+        self.src_x, self.src_y = self.own_src
+        self.n_src = self.src_x.size
+        self.nsx = self.src_x.shape
 
 class oKCSD3D(KCSD3D):
     """oKCSD - The variant for the Kernel Current Source Density method that 
@@ -1267,6 +1383,30 @@ class oKCSD3D(KCSD3D):
         self.estm_x, self.estm_y, self.estm_z = self.own_est
         self.src_x, self.src_y, self.src_z = self.own_src
         self.n_estm = self.estm_x.size
+        
+    def place_basis(self):
+        """Places basis sources of the defined type.
+
+        Checks if a given source_type is defined, if so then defines it
+        self.basis, This function gives locations of the basis sources,
+        Defines
+        source_type : basis_fuctions.basis_1D.keys()
+        self.R based on R_init
+        self.dist_max as maximum distance between electrode and basis
+        self.nsx = self.src_x.shape
+        self.src_x, self.src_y, self.src_z : Locations at which basis sources are placed.
+
+        """
+        source_type = self.src_type
+        try:
+            self.basis = basis.basis_1D[source_type]
+        except KeyError:
+            raise KeyError('Invalid source_type for basis! available are:',
+                           basis.basis_1D.keys())
+        self.R = self.R_init
+        self.src_x, self.src_y, self.src_z = self.own_src
+        self.n_src = self.src_x.size
+        self.nsx = self.src_x.shape
 
 if __name__ == '__main__':
     print('Checking 1D')
